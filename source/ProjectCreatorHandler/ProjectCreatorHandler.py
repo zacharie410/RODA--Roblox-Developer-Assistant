25705639fb2112f97dd3bcbb0bd13b6294abd103
Zacharie Fortin <zacharie410@gmail.com>
2023-07-13 16:38:16 -0400

    Project Creator Handler Subprocess Update

--- source/ProjectCreatorHandler/ProjectCreatorHandler.py
+++ source/ProjectCreatorHandler/ProjectCreatorHandler.py
@@ -18,8 +18,8 @@ class ProjectCreatorHandler:
         new_project = {'id': self.api.generate_unique_key(), 'name': name, 'description': description, 'file_path': file_path}
         os.makedirs(file_path)
         if init:
-            subprocess.run("rojo init "+sanitized, cwd=file_path1, shell=True)
-            subprocess.run("rojo build -o build.rbxlx", cwd=file_path, shell=True)
+            subprocess.run("rojo init "+sanitized, cwd=file_path1, shell=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            subprocess.run("rojo build -o build.rbxlx", cwd=file_path, shell=True, creationflags=subprocess.CREATE_NO_WINDOW)
         self.api.create_project(new_project)
 
         self.viewer.populate_project_list()
@@ -40,12 +40,3 @@ class ProjectCreatorHandler:
                 'file_path': updated_file_path if updated_file_path else project['file_path']
             }
             self.api.update_project(project_id, updated_project)
-        else:
-            print(f"Project with ID {project_id} does not exist.")
-    
-    def delete_project(self, project_id):
-        deleted = self.api.delete_project(project_id)
-        if deleted:
-            print(f"Project with ID {project_id} deleted successfully.")
-        else:
-            print(f"Failed to delete project with ID {project_id}.")
