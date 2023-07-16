b31a377d936f962a79d3951b020303571d161663
Zacharie Fortin <zacharie410@gmail.com>
2023-07-16 15:44:49 -0400

    mac-project-creator

--- source/ProjectCreatorHandler/ProjectCreatorHandler.py
+++ source/ProjectCreatorHandler/ProjectCreatorHandler.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import re
+import sys
 
 class ProjectCreatorHandler:
     def __init__(self, api, viewer):
@@ -18,8 +19,8 @@ class ProjectCreatorHandler:
         new_project = {'id': self.api.generate_unique_key(), 'name': name, 'description': description, 'file_path': file_path}
         os.makedirs(file_path)
         if init:
-            subprocess.run("rojo init "+sanitized, cwd=file_path1, shell=True, creationflags=subprocess.CREATE_NO_WINDOW)
-            subprocess.run("rojo build -o build.rbxlx", cwd=file_path, shell=True, creationflags=subprocess.CREATE_NO_WINDOW)
+            self.run_rojo_init(sanitized, file_path1)
+            self.run_rojo_build(file_path)
         self.api.create_project(new_project)
 
         self.viewer.populate_project_list()
@@ -40,3 +41,23 @@ class ProjectCreatorHandler:
                 'file_path': updated_file_path if updated_file_path else project['file_path']
             }
             self.api.update_project(project_id, updated_project)
+
+    def run_rojo_init(self, sanitized, file_path1):
+        if sys.platform == "win32":
+            command = f"rojo init {sanitized}"
+            subprocess.run(command, cwd=file_path1, shell=True, creationflags=subprocess.CREATE_NO_WINDOW, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        elif sys.platform == "darwin" or sys.platform.startswith("linux"):
+            command = f"rojo init {sanitized}"
+            subprocess.run(command, cwd=file_path1, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        else:
+            print("Unsupported operating system.")
+
+    def run_rojo_build(self, file_path):
+        if sys.platform == "win32":
+            command = "rojo build -o build.rbxlx"
+            subprocess.run(command, cwd=file_path, shell=True, creationflags=subprocess.CREATE_NO_WINDOW, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        elif sys.platform == "darwin" or sys.platform.startswith("linux"):
+            command = "rojo build -o build.rbxlx"
+            subprocess.run(command, cwd=file_path, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        else:
+            print("Unsupported operating system.")
